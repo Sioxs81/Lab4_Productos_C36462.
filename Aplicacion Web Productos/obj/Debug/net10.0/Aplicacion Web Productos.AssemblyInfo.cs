83cd3064c5340e5d6c7e67ca989e5777e689bddb
Sioxs81 <demiansand@gmail.com>
2026-04-17 18:22:06 -0600

    COMMIT: ARREGLADO EL ERROR DE GIT

--- Aplicacion Web Productos/obj/Debug/net10.0/Aplicacion Web Productos.AssemblyInfo.cs
+++ Aplicacion Web Productos/obj/Debug/net10.0/Aplicacion Web Productos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion Web Productos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3580bd649b32f1455eacfd0e997c4e29e584b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463b634e2e8743ef5b3c83ee52ca4b32118464e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion Web Productos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion Web Productos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
